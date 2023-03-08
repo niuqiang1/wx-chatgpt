f376240805e3ca1eae51c81258db3911a4dd797a
niuqiang <qiang.niu@leadbank.com.cn>
2023-03-08 11:20:53 +0800

    11

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
 
 const configuration = new Configuration({
-  apiKey: 'wx96fa34eb414ede31',
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
